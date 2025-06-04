d3dbd16716e33502c7490709115a28962b515b63
Clark Xu <yilinxu@brandeis.edu>
2025-06-04 19:34:20 +0800

    Update time_comparison.py

--- treeple/stats/time_comparison.py
+++ treeple/stats/time_comparison.py
@@ -125,7 +125,7 @@ def time_test(dim_range,sample_range):
 #dim = np.array([128, 256, 512, 1024, 2048, 4096, 8192])
 #dim2 = np.array([128, 256, 512, 1024, 2048, 4096])
 dim = [100]
-dim2 = [10]
+dim2 = [100]
 os.makedirs("./sex_classification/results", exist_ok=True)  # for p_values_*.npy
 os.makedirs("./sex_classification/results_permutation_testing", exist_ok=True)  # for time_list_*.npy
 
