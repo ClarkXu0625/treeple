ad7989460c7f696564673b073053a5f56275abd8
Yilin Xu <yxu233@jh.edu>
2025-06-13 10:06:50 -0400

    bash code to run might train cohort1

---
+++ clark/might_benchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Setup environment activation
+source ~/miniconda3/etc/profile.d/conda.sh
+
+# Define Python script path
+PY_SCRIPT="run_model_comparison.py"
+SCRIPT_DIR=$(dirname "$PY_SCRIPT")
+SCRIPT_BASE=$(basename "$PY_SCRIPT" .py)
+
+# Run in treeple_fast
+echo "=== Running in treeple_fast ==="
+conda activate treeple_fast
+python "$PY_SCRIPT" > "${SCRIPT_DIR}/${SCRIPT_BASE}_fast.txt" 2>&1
+conda deactivate
+
+# Run in treeple_standard
+echo "=== Running in treeple_standard ==="
+conda activate treeple_standard
+python "$PY_SCRIPT" > "${SCRIPT_DIR}/${SCRIPT_BASE}_standard.txt" 2>&1
+conda deactivate
