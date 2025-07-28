6e919cbcd2d758c2ff65d23834ac7c96aa5e221a
Yilin (Clark) Xu <clarkxu625@gmail.com>
2025-07-28 00:01:16 -0400

    Update test_time.py

--- treeple/stats/test_time.py
+++ treeple/stats/test_time.py
@@ -1,5 +1,6 @@
 from treeple.datasets import make_trunk_classification
 from sklearn.model_selection import train_test_split
+from neofit import NeuroExplainableOptimalFIT
 from neofit_ydf import NeuroExplainableOptimalFIT_ydf
 #from treeple.stats import NeuroExplainableOptimalFIT
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import numpy as np
 import pandas as pd
 import time
 import os
+import pdb
 
 
 
@@ -16,24 +18,42 @@ def time_test(dim_range,sample_range):
     sample_list = []
     for dim in dim_range:
         for sample in sample_range:
-            X, y = make_trunk_classification(n_samples=sample, n_dim=dim, n_informative=min(dim,600), seed=0)
+            X, y = make_trunk_classification(n_samples=sample, n_dim=dim, n_informative=min(dim,600), seed=1)
             X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=0)
-            X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=0)
+            X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=2)
             start_time = time.time()
 
-            # neofit testing
-            neofit = NeuroExplainableOptimalFIT_ydf(n_estimators=5000,n_permutations=100000,clf_type="SPORF",alpha=0.05, verbose=False)
-            p_values, imp_features, _ = neofit.get_significant_features(X_train, y_train)
-            end_time = time.time()
+            # neofit testing, CUDA version
+            neofit_cuda = NeuroExplainableOptimalFIT(n_estimators=5000,
+                                                    n_permutations=100000,
+                                                    clf_type="MORF",
+                                                    alpha=0.05, 
+                                                    verbose=False, 
+                                                    device='cuda')
+            p_values1, imp_features1, _ = neofit_cuda.get_significant_features(X_train, y_train)
+            end_time1 = time.time()
+
+            # Neofit cpu version
+            neofit = NeuroExplainableOptimalFIT(n_estimators=5000,
+                                                n_permutations=100000,
+                                                clf_type="MORF",
+                                                alpha=0.05, 
+                                                verbose=False, 
+                                                device='cpu')
+            p_values2, imp_features2, _ = neofit.get_significant_features(X_train, y_train)
+            end_time2 = time.time()
+
+            print(sum(imp_features2.astype(int)-imp_features1.astype(int)))
             # save results of each run
             #os.makedirs("./sex_classification/results/", exist_ok=True)
             #np.save(f"./sex_classification/results/p_values_{sample}_{dim}.npy", p_values)
-
-            time_list.append(end_time - start_time)
+            #pdb.set_trace()
+            time_list.append(end_time1 - start_time)
             #with open("time_log.txt", "a") as log_file:
             #    log_file.write(f"Time taken for sample {sample} and dim {dim} is: {end_time - start_time:.2f} seconds\n")
             dim_list.append(dim)
             sample_list.append(sample)
+
     # save time_list, dim_list and sample_list
     time_list = np.array(time_list)
     dim_list = np.array(dim_list)
@@ -48,13 +68,14 @@ def time_test(dim_range,sample_range):
 
 
 #dim = range(1000,6000,1000)
-#dim = np.array([128, 256, 512, 1024, 2048, 4096, 8192])
-dim = np.array([124])
+#dim = np.array([128,256,512,1024,2048])
+dim = np.array([1024])
+#dim = np.array([124])
 sample = dim
 #sample = range(1000,6000,1000)
 results_time = time_test(dim, sample)
 print(results_time)
 
 # save results to a csv file
-#results_time.to_csv('time_results_fast.csv', index=False)
+results_time.to_csv('time_results_fast.csv', index=False)
   
